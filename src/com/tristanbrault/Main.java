5830e3210f34deb42aa0fcb07b25cc55a9b57857
tristan1940252 <trit0tv0@gmail.com>
2020-09-03 18:18:04 -0400

    avancer

--- src/com/tristanbrault/Main.java
+++ src/com/tristanbrault/Main.java
@@ -9,7 +9,7 @@ public class Main {
         RandomAccessFile file = null;
         String fileName = "default";
         long offset = 0;
-        long length = 0;
+        long length = -1;
         long stringLength = 4;
         boolean fileExist = true;
         boolean text = false;
@@ -18,47 +18,53 @@ public class Main {
             if (args[i].equals("-f")) {
                 fileName = args[++i];
                 System.out.println(fileName + " filename");
+                try{
+                    file = new RandomAccessFile(fileName,"r");
+                }
+                catch (FileNotFoundException e){
+                    printUsage();
+                    fileExist = false;
+                }
             }
-            try{
-                file = new RandomAccessFile(fileName,"r");
-            }
-            catch (FileNotFoundException e){
-                printUsage();
-                fileExist = false;
-            }
-            //if (fileExist){
-                length = file.length();
-                if(args[i].equals("-o")){
-                    if ((Long.parseLong(args[i+1]) >= 0) && (Long.parseLong(args[i+1]) < file.length())){
-                        offset = Long.parseLong(args[i+1]);
-                    }
-                    System.out.println(offset + " offset");
-                }
-                else if (args[i].equals("-l")){
-                    if (Long.parseLong(args[i+1]) > 0 && Long.parseLong(args[i+1]) < (file.length() - offset)){
-                        length = Long.parseLong(args[i+1]);
-                    }
-                    System.out.println(length + " length");
-                }
-                else if(args[i].equals("-s")){
-                    text = true;
-                    try{
-                        stringLength = Long.parseLong(args[i+1]);
-                    }
-                    catch (Exception e){
-                        stringLength = 4;
-                    }
-                }
-                else{
-                    text = false;
-                }
-            //}
+
         }
-        if (text){
-            printText(file,offset,length,stringLength);
+        if (fileExist){
+            for (int i = 0; i < args.length; i = i +2) {
+                switch (args[i]) {
+                    case "-o" -> {
+                        if ((Long.parseLong(args[i + 1]) >= 0) && (Long.parseLong(args[i + 1]) < file.length())) {
+                            offset = Long.parseLong(args[i + 1]);
+                        }
+                        System.out.println(offset + " offset");
+                    }
+                    case "-l" -> {
+                        if (Long.parseLong(args[i + 1]) > 0 && Long.parseLong(args[i + 1]) < (file.length() - offset)) {
+                            length = Long.parseLong(args[i + 1]);
+                        }
+                        System.out.println(length + " length");
+                    }
+                    case "-s" -> {
+                        text = true;
+                        try {
+                            stringLength = Long.parseLong(args[i + 1]);
+                        } catch (Exception e) {
+                            stringLength = 4;
+                        }
+
+                    }
+
+                }
+            }
+            if (length == -1){
+                length = file.length() - offset;
+            }
+            if (text){
+                printText(file,offset,length,stringLength);
+            }
+            else
+                printData(file, offset, length);
         }
-        else
-            printData(file, offset, length);
+
     }
 
     public static void printData(RandomAccessFile file, long offset, long length) throws IOException {
@@ -103,29 +109,37 @@ public class Main {
         System.out.println("-o est la position de départ (facultatif)");
         System.out.println("-l est le nombre de caractère à observer (facultatif)");
         System.out.println("-f est le nom du fichier à lire (obligatoire)");
+        System.out.println("-s est tranforme l'hexadecimal en chaine de charactère");
+
     }
 
-    public static void printText(RandomAccessFile file, long offset, long length, long max) throws IOException{
-        StringBuilder lolipop = new StringBuilder();
+    public static void printText(RandomAccessFile file, long offset, long length, long min) throws IOException{
+        String chaine = "";
         for (long i = offset; i < length; i++){
             file.seek(i);
             int chiffre = file.read();
             //if((chiffre >= 48 && chiffre <= 57) || (chiffre >= 65 && chiffre <= 90) || (chiffre >= 97 && chiffre <= 122) || chiffre == 32){
             if (chiffre > 7 && chiffre < 128){
                 //System.out.print("\u001B[37m" + (char)chiffre);
-                lolipop.append((char) chiffre);
+                chaine += (char) chiffre;
             }
             else{
-                //System.out.print(".");
-                if (chiffre == 0 && lolipop.length() > max){
-                    System.out.print(lolipop);
+//                System.out.print("\u001B[33m" + Long.toHexString(chiffre) + " ");
+                if (chiffre == 0 && chaine.length() >= min){
+                    System.out.println(chaine);
+
                 }
-                else{
-                    lolipop = new StringBuilder();
-                }
+                chaine = "";
             }
         }
         System.out.println();
+    }
+
+    public static void printInfos(RandomAccessFile file) throws IOException{
+        String signature = "PE\0\0";
+        for(int i = 0; i < file.length(); i++){
+
+        }
     }
 
 
@@ -134,10 +148,10 @@ public class Main {
         String hex =  String.valueOf(address / 16);
         switch (hex.length()){
             case 1: hexA = "0x0000" + hex + "0 "; break;
-            case 2: hexA = "0x000" + hex + "00 "; break;
-            case 3: hexA = "0x00" + hex + "000 "; break;
-            case 4: hexA = "0x0" + hex + "0000 "; break;
-            case 5: hexA = "0x" + hex + "00000 "; break;
+            case 2: hexA = "0x000" + hex + "0 "; break;
+            case 3: hexA = "0x00" + hex + "0 "; break;
+            case 4: hexA = "0x0" + hex + "0 "; break;
+            case 5: hexA = "0x" + hex + "0 "; break;
         }
         hexA = "\u001B[33m" + hexA;
         return hexA;
